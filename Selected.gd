cc4bddce0dedfc70aeca4ff025e60f891db0f95c
Vascoengif <vascobarnabe2000@gmail.com>
2019-11-10 22:51:42 +0000

    rebase all the work

--- Selected.gd
+++
@@ -1,9 +0,0 @@
-extends VBoxContainer
-
-func _ready():
-	pass # Replace with function body.
-func _process(delta):
-	if Input.is_key_pressed(KEY_LEFT):
-		self.position.y -=10
-	if Input.is_key_pressed(KEY_RIGHT):
-		self.position.y +=10
